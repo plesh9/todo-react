2873dcb021f7cdb7ef257b18a4127b01cb24cc41
Vadym <124910250+plesh9@users.noreply.github.com>
2023-09-07 01:11:46 +0300

    deploy gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Todo from './components/TodoList/Todo';
 import './scss/index.scss';
 
 function App() {
+  
   return (
     <div className="App">
       <Typography variant="h4" component="h1" fontWeight={700} mt={5} align='center'>
